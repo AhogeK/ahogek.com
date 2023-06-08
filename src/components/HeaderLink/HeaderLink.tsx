674bf7feb14776e393b18e0d5574e04352f3118a
AhogeK <ahogek@gmail.com>
2023-06-08 23:44:06 +0800

    feat(导航头): 首页 nav link

---
+++ src/components/HeaderLink/HeaderLink.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import {Link, useLocation} from "react-router-dom"
+import classNames from 'classnames'
+import './HeaderLink.css'
+
+interface HeaderLinkProps {
+  to: string
+  children: React.ReactNode
+  target?: string
+  className?: string
+}
+
+const HeaderLink: React.FC<HeaderLinkProps> = ({
+                                                 to, children, target,
+                                                 className
+                                               }) => {
+  const location = useLocation();
+  const isActive = to === location.pathname || to === location.pathname.replace(/\/$/, '');
+
+  const classList = classNames(className, {active: isActive});
+
+  return (
+    <Link to={to} target={target} className={classList}>
+      {children}
+    </Link>
+  )
+}
+
+export default HeaderLink;
